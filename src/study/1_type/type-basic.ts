867e91d204eff920ccc41109dfaaeaeda3675be9
devjun10 <devjun1023@gmail.com>
2023-04-02 02:43:48 +0900

    ts type 학습
    - ts 기본 타입 정리

---
+++ src/study/1_type/type-basic.ts
@@ -0,0 +1,26 @@
+// String
+var introduction: string = 'hello'
+
+// Number
+let num: number = 3;
+
+// Array
+let numbers: Array<number> = [1, 2, 3]
+let strings: Array<string> = ['A', 'B', 'C']
+let items: number[] = [1, 2, 3]
+
+// Tuple
+let address: [string, number] = ['SEOUL', 21];
+
+// Object
+let objects: object = {};
+let person: {
+    name: string,
+    age: number
+} = {
+    name: 'Jun',
+    age: 99
+}
+
+// Boolean
+let show: boolean = true;
