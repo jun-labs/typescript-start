3b1d25166646490ad25b869df6ae736a472f9f8b
devjun10 <devjun1023@gmail.com>
2023-04-03 04:17:28 +0900

    type-alias 테스트 코드 작성
    - 타입은 체크하지 않기 때문에 테스트 코드 주석 처리

---
+++ src/test/6_type-alias/type-alias.test.ts
@@ -0,0 +1,9 @@
+import {Cat} from "../../study/6_type-alias/type-alias";
+
+/**
+ *  참조: https://stackoverflow.com/questions/64030428/jest-typescript-check-for-type
+ *  타입 체크에 대한 테스트는 지양한다.
+ * */
+//describe('브랜드 포함 테스트', () => {
+//    const cat: Cat = {name: '냥냥이'}
+//})
