ca9a33245663322165d45920285c44f6b2c84f7f
devjun10 <devjun1023@gmail.com>
2023-04-03 04:27:44 +0900

    ts 타입 추론 학습
    - ts 타입 추론 학습
    - export 문법 학습

---
+++ src/study/12_type-inference/type-inference.ts
@@ -0,0 +1,13 @@
+function getString(str: string) {
+    return str;
+}
+
+function getNumber(num: number) {
+    return num;
+}
+
+function getBoolean(bool: boolean) {
+    return bool;
+}
+
+export {getString, getNumber, getBoolean};
